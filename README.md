ecb6b9cf6472cb2097f40a0e958e040572f1d6fa
Cody Garcia <codygarcia0923@gmail.com>
2023-05-10 11:15:11 -0400

    Idk anymore

---
+++ README.md
@@ -0,0 +1,79 @@
+# Matching-Card-Game
+
+# Match The House Down Boots
+
+## Date: May the Fourth be with you
+
+### By: Cody Garcia
+
+[www.codyrgarcia.com](https://www.codyrgarcia.com/)
+
+[@popgoesthecultureshock](https://www.instagram.com/popgoesthecultureshock/)
+
+---
+
+### **_Description_**
+
+In a world where drag is on the forefront of issues to be addressed by certain polititians, outreach and representation matter.
+Drag is not evil. While some drag queens are not good... _you know who you are_... They still have the right to be who they are, just like any citizen.
+This matching game honors the moving-to-mainstream art form that is drag, and just some of the queens that emerged from the 14 seasons of RuPauls Drag Race. Heard of it?
+
+This is a memory matching card game that users can play to see some of their favorite queens, and compete with each other with their high scores, or low score: how few turns it takes them.
+Chantay you stay through all three levels of 24, 30, and 36 cards in less turns than your friends, or sashay away.
+Spread the joys drag can create sharing this child friendly game with your friends and family!
+
+---
+
+### **_On The Day_**
+
+"It's the day of the show y'all." -Libby Mae Brown
+
+- Opening page of the Werk Room, welcoming the user and containing with links to the three different levels. Or one link to start the game, and the levels happen, one after the other.
+
+- Each game play takes place in the center of the screen, with a move tally on the left, and random words of encouragement from the show on the right.
+  - 24 / 6 by 4
+  - 30 / 6 by 5
+  - 36 / 6 by 6
+
+**Quotes to be randomly cycled**:
+
+-"Don't get bitter, just get better."
+
+-I feel like you’re being sabotaged by your inner saboteur.”
+
+-“It’s okay to fall down. Get up, look sickening, and make them eat it!”
+
+-“I’m not going to panic because I don’t do that anymore. It’s going to be okay.”
+
+-"When the going gets tough, the tough reinvent themselves."
+
+-"She already done had herses."
+
+### **_Screenshots_**
+
+![Image](https://pbs.twimg.com/media/EUYZE8qXsAMnDaH?format=jpg&name=large)
+
+![Image](https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/6cf3dcf4-3e01-439f-8e79-bc764099ba54/dc0rthv-3d4c44dc-056b-4afa-beff-64c9b64b80cb.jpg)
+
+![Image](https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/6cf3dcf4-3e01-439f-8e79-bc764099ba54/dbzh4zk-faea675c-f2a4-4ff7-b823-45e45ec63578.jpg)
+
+<!-- ![Image](https://www.figma.com/proto/HMVcjAxxOUraVqH5JQAQ3u/Match-the-House-Down-Boots?type=design&node-id=3-4&scaling=scale-down&page-id=0%3A1) -->
+
+- [x] README
+- [x] Pseudocode
+- [x] Wireframing
+- [ ] Trello
+- [ ] Or even ==Highlight== Items
+
+**_Credits_**
+
+Images:
+[DeviantArt](https://www.deviantart.com/yudelrey/art/Drag-Cards-RuPaul-s-Drag-Race-Season-5-724714112)
+
+[Drag RaceTwitter](https://twitter.com/RuPaulsDragRace/status/1244987814426693632/photo/1)
+
+[Quotes](https://inews.co.uk/culture/television/rupauls-drag-race-best-quotes-catchphrases-jokes-uk-series-345239)
+
+[Markdown Guide](https://ia.net/writer/support/general/markdown-guide)
+
+[Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)
---
+++ journey.js
@@ -0,0 +1,153 @@
+// -------constants----------
+const quotes = [
+  `Don't get bitter, just get better.`,
+  `I feel like you’re being sabotaged by your inner saboteur.`,
+  `It’s okay to fall down. Get up, look sickening, and make them eat it!`,
+  `I’m not going to panic because I don’t do that anymore. It’s going to be okay.`,
+  `When the going gets tough, the tough reinvent themselves.`,
+  `She already done had herses.`
+]
+
+const cards = [
+  { name: 'd1', img: 'Matching-Card-Game/Images/Alaska.png' },
+  { name: 'd2', img: 'Matching-Card-Game/Images/AlyssaEdwards.png' },
+  { name: 'd3', img: 'Matching-Card-Game/Images/CocoMontrese.png' },
+  { name: 'd4', img: 'Matching-Card-Game/Images/Detox.png' },
+  { name: 'd5', img: 'Matching-Card-Game/Images/Eureka.png' },
+  { name: 'd6', img: 'Matching-Card-Game/Images/IvyWinters.png' },
+  { name: 'd7', img: 'Matching-Card-Game/Images/JinkxMonsoon.png' },
+  { name: 'd8', img: 'Matching-Card-Game/Images/Lineysha.png' },
+  { name: 'd9', img: 'Matching-Card-Game/Images/Roxxxy.png' },
+  { name: 'd10', img: 'Matching-Card-Game/Images/SashaVelour.png' },
+  { name: 'd11', img: 'Matching-Card-Game/Images/SheaCoulee.png' },
+  { name: 'd12', img: 'Matching-Card-Game/Images/TheTuck.png' }
+  // { name: 'd1', img: 'Matching-Card-Game/Images/Aja.png' },
+  // { name: 'd7', img: 'Matching-Card-Game/Images/Honey.png' },
+  // { name: 'd9', img: 'Matching-Card-Game/Images/Jade.png' },
+  // { name: 'd12', img: 'Matching-Card-Game/Images/Penny.png' },
+  // { name: 'd15', img: 'Matching-Card-Game/Images/SerenaChaCha.png' },
+  // { name: 'd18', img: 'Matching-Card-Game/Images/Vivienne.png' }
+]
+// Audio?
+
+// -------apps state (variables)---------
+let front
+let back
+// let points
+let click = 0
+let firstCard
+let secondCard
+let tally
+//there isn't going to be a winner, its just about how few turns you can do it in
+// -------cached element references---------
+const CardBoard = document.querySelector('#CardBoard')
+let turnTally = document.querySelector('.tally') // tally for the amount of turns the user beat the level in
+
+const cardsAvailable = [...cards, ...cards]
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex]
+    ]
+  }
+
+  return array
+}
+shuffle(cardsAvailable)
+
+let playingCards
+
+const totalCards = cardsAvailable.length
+const individual = () => {
+  front = document.querySelector('.FRONT')
+  back = document.querySelectorAll('.BACK')
+}
+
+// -------functions----------
+const init = () => {
+  tally = 0
+  // points = 0
+  board = null
+  activeTurn = false
+  // activeCards = null
+  click = 0
+}
+
+const cardsFront = () => {
+  for (let i = 0; i < cards.length; i++) {
+    document.querySelector('.FRONT').innerHTML = `<img src=${cards[i].img}/>`
+  }
+}
+
+const randomQuote = () => {
+  document.querySelector('.quote').textContent = `"${
+    quotes[Math.floor(Math.random() * quotes.length)]
+  }"`
+}
+randomQuote()
+
+const createCards = () => {
+  cardsAvailable.forEach((el) => {
+    const card = document.createElement('div')
+    card.className = 'TheCards'
+    card.innerHTML = `
+    <div class="FRONT">
+    <img class="frontIMG" src="${el.img}"/>
+    </div>
+    <div class="BACK">
+    <img class="backIMG" id = "${el.name}" src="Matching-Card-Game/Images/BACK.png"/>
+    </div>`
+    CardBoard.append(card)
+  })
+  individual()
+}
+createCards() // creat a div for every card you have in that cards available Array, take the variable you assigned the div to to .HTML.
+
+const renderTally = () => {
+  const score = turnTally
+  score.innerText = `Tally ${(tally += 1)}`
+}
+
+const playerTurn = (evt) => {
+  click++
+  console.log(evt.target.parentNode)
+  console.log('secondCard', secondCard)
+  if (click === 1) {
+    firstCard = evt.target
+  } else if (click === 2) {
+    secondCard = evt.target
+    if (firstCard.id !== secondCard.id) {
+      setTimeout(() => {
+        firstCard.parentNode.classList.remove('hiddenClass')
+        secondCard.parentNode.classList.remove('hiddenClass')
+      }, 1000)
+      click = 0
+    } else if (firstCard.id === secondCard.id) {
+      // points++
+      addClass = 'match'
+      click = 0
+    }
+    renderTally()
+  }
+}
+// for when the card is clicked, it will turn over, revealing its face to the user.
+// The card will stay turned over until a second card is clicked and turned over. When two cards get clicked, a point will be added to the tally board.
+// If the numbers on the cards are the SVGMaskElement, the cards will stay flipped over. If the numbers are different, the cards will turn back around.
+// ONce all the cards are turned over (all matches have been made), the game will be finished.
+// Each time the game SVGTransformList, the cards will be randomly set to each spot.
+// -------event listeners----------
+back.forEach((card) => {
+  card.addEventListener('click', function (evt) {
+    console.log(evt.target)
+    card.classList.add('hiddenClass')
+    console.log(card)
+    playerTurn(evt)
+  })
+})
+// listen for a click on any card. Then any other card. Then after the second click, turn both cards back around if they dont matchMedia.
+init()
